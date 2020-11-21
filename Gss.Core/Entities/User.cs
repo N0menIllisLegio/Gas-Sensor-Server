2721cf208c412addd4846d27187193d8bbda76d7
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2020-11-22 02:47:42 +0300

    Add personal info to User
    Seed administrator

--- Gss.Core/Entities/User.cs
+++ Gss.Core/Entities/User.cs
@@ -6,6 +6,13 @@ namespace Gss.Core.Entities
 {
   public class User : IdentityUser<Guid>
   {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string AvatarPath { get; set; }
+
+    public string Gender { get; set; }
+    public DateTime? Birthday { get; set; }
+
     // ------ RELATIONSHIPS
 
     public ICollection<Microcontroller> Microcontrollers { get; set; }
