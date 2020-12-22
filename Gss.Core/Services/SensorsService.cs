0503ed8b9e7e712ca16b046121c458e6d0fb7c32
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2020-12-22 21:56:21 +0300

    Create SensorsServce and interface for DI

---
+++ Gss.Core/Services/SensorsService.cs
@@ -0,0 +1,14 @@
+﻿using Gss.Core.Interfaces;
+
+namespace Gss.Core.Services
+{
+  public class SensorsService : ISensorsService
+  {
+    private readonly ISensorsRepository _sensorsRepository;
+
+    public SensorsService(ISensorsRepository sensorsRepository)
+    {
+      _sensorsRepository = sensorsRepository;
+    }
+  }
+}
