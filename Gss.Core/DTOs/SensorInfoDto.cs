af74935445d361170f57b13bef177e4102b6a4df
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2020-12-22 21:55:35 +0300

    Add Sensors request/response dtos

---
+++ Gss.Core/DTOs/SensorInfoDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Gss.Core.DTOs
+{
+  public class SensorInfoDto
+  {
+    public Guid ID { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    // TODO Add SensorTypeDto
+  }
+}
