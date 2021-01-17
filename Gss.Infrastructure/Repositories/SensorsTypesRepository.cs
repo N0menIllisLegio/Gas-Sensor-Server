09260b4b19b92d4bd0877b2ffa1b1248d4535bbd
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-01-17 23:53:04 +0300

    Remove redundant usings

--- Gss.Infrastructure/Repositories/SensorsTypesRepository.cs
+++ Gss.Infrastructure/Repositories/SensorsTypesRepository.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Gss.Core.DTOs;
 using Gss.Core.Entities;
-using Gss.Core.Enums;
 using Gss.Core.Helpers;
 using Gss.Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
