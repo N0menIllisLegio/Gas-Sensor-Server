2ca6c493178a685e04a5a5605026d5240bf52737
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-02-05 01:40:08 +0300

    Add requested sensor id migration

---
+++ Gss.Infrastructure/Migrations/20210129200527_AddRequestedSensorIDColumnMigration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gss.Infrastructure.Migrations
+{
+    public partial class AddRequestedSensorIDColumnMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "RequestedSensorID",
+                table: "Microcontrollers",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RequestedSensorID",
+                table: "Microcontrollers");
+        }
+    }
+}
