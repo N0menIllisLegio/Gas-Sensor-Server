85d8ad9223de7ea09b0eb88b35dade8d613d0890
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-04-25 18:57:47 +0300

    Generalize users table and move it to separate component
    Remove redundant MailIcon

--- Gss.Web/ClientApp/src/components/ClippedDrawer.js
+++ Gss.Web/ClientApp/src/components/ClippedDrawer.js
@@ -8,7 +8,6 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import { Link } from "react-router-dom";
 import PeopleAltTwoToneIcon from '@material-ui/icons/PeopleAltTwoTone';
 
