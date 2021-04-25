f3e759d29eeae150f6807f022543209eeb6a5c29
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-04-25 03:16:03 +0300

    Create ClippedDrawer

---
+++ Gss.Web/ClientApp/src/components/ClippedDrawer.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import { Link } from "react-router-dom";
+import PeopleAltTwoToneIcon from '@material-ui/icons/PeopleAltTwoTone';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerContainer: {
+    overflow: 'auto',
+  },
+  link: {
+    color: 'inherit',
+    textDecoration: 'none',
+  }
+}));
+
+export default function ClippedDrawer() {
+  const classes = useStyles();
+
+  return (
+    <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            <Link to="/users" key="users" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon><PeopleAltTwoToneIcon /></ListItemIcon>
+                <ListItemText primary="Users" />
+              </ListItem>
+            </Link>
+          </List>
+        </div>
+      </Drawer>
+  );
+}
