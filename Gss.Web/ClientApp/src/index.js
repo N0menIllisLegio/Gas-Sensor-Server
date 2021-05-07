1514726a70b2235082603051889b6475ad72c81f
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-05-08 02:57:36 +0300

    Add provider

--- Gss.Web/ClientApp/src/index.js
+++ Gss.Web/ClientApp/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import '@fontsource/roboto';
 import './index.css';
+import { Provider } from 'react-redux'
+import store from './redux/store';
 //import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
 
