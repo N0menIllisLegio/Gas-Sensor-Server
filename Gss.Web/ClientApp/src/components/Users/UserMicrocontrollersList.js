63e9a54f0d291eb19a9d343815433e646aa47f00
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-05-18 00:58:41 +0300

    Display microcontrollers in user's profile

--- Gss.Web/ClientApp/src/components/Users/UserMicrocontrollersList.js
+++ Gss.Web/ClientApp/src/components/Users/UserMicrocontrollersList.js
@@ -1,16 +1,164 @@
+import { useEffect, useState } from "react";
+import { usePagedPost } from "../../hooks/usePost";
+import Progress from "../Progress";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { FormControl, Grid, InputLabel, MenuItem, Select, TextField } from "@material-ui/core";
+import DnsTwoToneIcon from '@material-ui/icons/DnsTwoTone';
+import AlarmTwoToneIcon from '@material-ui/icons/AlarmTwoTone';
+import LocationOnTwoToneIcon from '@material-ui/icons/LocationOnTwoTone';
+import VpnLockTwoToneIcon from '@material-ui/icons/VpnLockTwoTone';
+import lightGreen from '@material-ui/core/colors/lightGreen';
+import red from '@material-ui/core/colors/red';
+import { Link } from "react-router-dom";
+import { Pagination } from "@material-ui/lab";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  },
+  card: {
+    width: '600px',
+    margin: theme.spacing(2),
+    cursor: 'pointer'
+  },
+  title: {
+    fontSize: 17,
+    marginBottom: theme.spacing(2)
+  },
+  inputsRow: {
+    margin: theme.spacing(2),
+    display: 'flex',
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  }
+}));
 
-export default function UserMicrocontrollersList() {
-  const array = new Array(100);
-  array.fill(1, 0, -1)
+const dateTimeOptions = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  weekday: 'long',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  hour12: false
+};
+
+export default function UserMicrocontrollersList(props) {
+  const classes = useStyles();
+  const [searchString, setSearchString] = useState('');
+  const [totalPagesNumber, setTotalPagesNumber] = useState(1);
+  const [currentPageNumber, setCurrentPageNumber] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [userMicrocontrollersUrl, setUserMicrocontrollersUrl] = useState(`${process.env.REACT_APP_SERVER_URL}api/Microcontrollers/GetUserMicrocontrollers/${props.userID}`);
+  const { data: pagedResponse, isPending } = usePagedPost(userMicrocontrollersUrl, currentPageNumber, pageSize, searchString);
+
+  const [microcontrollers, setMicrocontrollers] = useState(null);
+
+  console.log(pageSize, pageSize);
+  
+  useEffect(() => {
+    if (pagedResponse != null) {
+      setMicrocontrollers(pagedResponse.Items);
+      setTotalPagesNumber(Math.ceil(pagedResponse.TotalItemsCount / pagedResponse.PagedInfo.PageSize));
+      console.log(microcontrollers);
+    }
+  }, [pagedResponse]);
+
+  useEffect(() => {
+    setCurrentPageNumber(1);
+  }, [searchString]);
   
   return (
     <div>
-      {array.map(el => (<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus minus pariatur eligendi quis, commodi nemo eos magni.
-      Consequuntur dolorum, est praesentium, delectus deserunt porro iusto harum magni saepe assumenda accusamus.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit illum vitae voluptates sint iusto et commodi, eos rerum, inventore est sunt autem, enim nam odio debitis eveniet minus voluptatem aut!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, incidunt numquam. Consectetur deserunt voluptates eius rem nisi error beatae magnam, omnis voluptatum nulla ab corrupti voluptatibus. Maiores, et laborum! Eveniet!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ullam quae, maxime reiciendis, quis amet deserunt optio earum sed impedit a soluta sit dolorem totam temporibus. Iure expedita voluptatibus explicabo!</p>))}
+      { isPending && (<Progress />) }
+      { microcontrollers && (
+        <div>
+          <div className={classes.inputsRow}>
+            <TextField label="Search..." value={searchString} onChange={(e) => setSearchString(e.target.value)}/>
+
+            {totalPagesNumber > 1 &&
+              (
+                <Pagination
+                  variant="outlined"
+                  color="primary"
+                  count={totalPagesNumber}
+                  page={currentPageNumber}
+                  onChange={(e, pageNumber) => setCurrentPageNumber(pageNumber)} />
+              )
+            }
+
+            <FormControl className={classes.formControl}>
+              <InputLabel>MCs</InputLabel>
+              <Select label="MCs"
+                value={pageSize}
+                onChange={(e) => setPageSize(e.target.value)}>
+                <MenuItem value={10}>10</MenuItem>
+                <MenuItem value={20}>20</MenuItem>
+                <MenuItem value={30}>30</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className={classes.root}>
+            <MicrocontrollersCardsList microcontrollers={microcontrollers} classes={classes} />
+          </div>
+        </div>
+      )}
     </div>
+  );
+}
+
+function MicrocontrollersCardsList(props) {
+  const classes = props.classes;
+  
+  return props.microcontrollers && props.microcontrollers.map(microcontroller => (
+    <Link to={`/microcontroller/${microcontroller.ID}`} key={microcontroller.ID} style={{ textDecoration: 'none' }}>
+      <Card className={classes.card}>
+        <CardContent>
+          <Grid container>
+            <Grid item xs={12}>
+              <Typography className={classes.title} color="textSecondary" gutterBottom>
+                {microcontroller.Name}
+              </Typography>
+            </Grid>
+
+            <MicrocontrollerDetail icon={(<DnsTwoToneIcon />)} name="IP" content={microcontroller.IPAddress} />
+            <MicrocontrollerDetail icon={(<AlarmTwoToneIcon />)} name="Last Response Time" content={new Date(microcontroller.LastResponseTime).toLocaleString("en-US", dateTimeOptions)} />
+            <MicrocontrollerDetail icon={(<LocationOnTwoToneIcon />)} name="Position" content={(<span><b>lat:</b> {microcontroller.Latitude || '—'} <b>long:</b> {microcontroller.Longitude || '—'}</span>)} />
+            <MicrocontrollerDetail icon={(<VpnLockTwoToneIcon />)} name="Privacy" content={(<span style={{color: microcontroller.Public ? lightGreen[500] : red[300]}}>{microcontroller.Public ? 'Public' : 'Private'}</span>)} />
+          </Grid>
+        </CardContent>
+      </Card>
+    </Link>
+  ));
+}
+
+function MicrocontrollerDetail(props) {
+  return (
+    <Grid container>
+      <Grid item xs={1}>
+        {props.icon}
+      </Grid>
+      <Grid item xs={4}>
+        <Typography>
+          {props.name}:
+        </Typography>
+      </Grid>
+      <Grid item xs={7}>
+        <Typography style={{textAlign: 'right'}}>
+          {props.content || '—'}
+        </Typography>
+      </Grid>
+    </Grid>
   );
 }
