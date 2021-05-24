f30aef467c67f81b95a151576b409bf245432c1b
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-05-25 00:58:16 +0300

    Add query to DB which extracts currently visible microcontrollers on map

--- Gss.Core/Interfaces/Repositories/IMicrocontrollersRepository.cs
+++ Gss.Core/Interfaces/Repositories/IMicrocontrollersRepository.cs
@@ -1,7 +1,11 @@
-﻿using Gss.Core.Entities;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Gss.Core.Entities;
 
 namespace Gss.Core.Interfaces.Repositories
 {
   public interface IMicrocontrollersRepository: IRepositoryBase<Microcontroller>
-  { }
+  {
+    Task<List<Microcontroller>> GetVisibleMicrocontrollers(double southWestLatitude, double southWestLongitude, double northEastLatitude, double northEastLongitude);
+  }
 }
--- Gss.Infrastructure/Repositories/MicrocontrollersRepository.cs
+++ Gss.Infrastructure/Repositories/MicrocontrollersRepository.cs
@@ -1,5 +1,9 @@
-﻿using Gss.Core.Entities;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gss.Core.Entities;
 using Gss.Core.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gss.Infrastructure.Repositories
 {
@@ -8,5 +12,16 @@ namespace Gss.Infrastructure.Repositories
     public MicrocontrollersRepository(AppDbContext appDbContext)
       : base(appDbContext)
     { }
+
+    public async Task<List<Microcontroller>> GetVisibleMicrocontrollers(double southWestLatitude, double southWestLongitude,
+      double northEastLatitude, double northEastLongitude)
+    {
+      return await DbSet.Where(mc => mc.Latitude.HasValue && mc.Longitude.HasValue
+        && mc.Latitude > southWestLatitude && mc.Latitude < northEastLatitude
+        && mc.Longitude > southWestLongitude && mc.Longitude < northEastLongitude)
+        .OrderBy(mc => mc.Name)
+        .Take(100)
+        .ToListAsync();
+    }
   }
 }
