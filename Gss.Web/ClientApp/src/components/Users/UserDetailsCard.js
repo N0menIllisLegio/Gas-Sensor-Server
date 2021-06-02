9a9359492798bacafe384f714030056b369f5f51
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-06-03 01:00:53 +0300

    Remove redundant image title

--- Gss.Web/ClientApp/src/components/Users/UserDetailsCard.js
+++ Gss.Web/ClientApp/src/components/Users/UserDetailsCard.js
@@ -57,8 +57,7 @@ export default function UserDetailsCard(props) {
       <Card className={classes.root}>
         <CardMedia
           className={classes.media}
-          image={user.AvatarPath != null ? user.AvatarPath : process.env.REACT_APP_AVATAR_PLACEHOLDER_URL}
-          title="Contemplative Reptile" />
+          image={user.AvatarPath != null ? user.AvatarPath : process.env.REACT_APP_AVATAR_PLACEHOLDER_URL} />
         <CardContent style={{padding: '16px'}}>
           <div className={classes.namesHeader}>
             <Typography gutterBottom variant="h5" component="h2">
