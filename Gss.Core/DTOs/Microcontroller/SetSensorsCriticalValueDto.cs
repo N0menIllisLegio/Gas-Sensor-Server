7f5e34c14f4e0b3237cc9b6ff5e77f53750e4559
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-06-05 02:48:30 +0300

    Add dto

---
+++ Gss.Core/DTOs/Microcontroller/SetSensorsCriticalValueDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gss.Core.DTOs.Microcontroller
+{
+  public class SetSensorsCriticalValueDto
+  {
+    [Required]
+    public Guid MicrocontrollerID { get; set; }
+
+    [Required]
+    public Guid SensorID { get; set; }
+
+    public int? CriticalValue { get; set; }
+  }
+}
