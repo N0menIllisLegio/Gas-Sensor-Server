d2106ff8ed1d5454c8d7142f3fa340f3ba899bf5
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-07-06 16:45:10 +0300

    Remove redundant comments

--- Gss.Infrastructure/UnitOfWork.cs
+++ Gss.Infrastructure/UnitOfWork.cs
@@ -53,30 +53,23 @@ namespace Gss.Infrastructure
       {
         if (disposing)
         {
-          // TODO: dispose managed state (managed objects).
           _context.Dispose();
         }
-
-        // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-        // TODO: set large fields to null.
 
         _disposedValue = true;
       }
     }
 
-    // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+    // Override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
     // ~UnitOfWork()
     // {
-    //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
     //   Dispose(false);
     // }
 
-    // This code added to correctly implement the disposable pattern.
     public void Dispose()
     {
-      // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
       Dispose(true);
-      // TODO: uncomment the following line if the finalizer is overridden above.
+      // Uncomment the following line if the finalizer is overridden above.
       // GC.SuppressFinalize(this);
     }
   }
--- Gss.Web/Startup.cs
+++ Gss.Web/Startup.cs
@@ -71,7 +71,7 @@ namespace Gss.Web
       if (!env.IsDevelopment())
       {
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        // TODO app.UseHsts();
+        app.UseHsts();
       }
 
       app.UseMiddleware<EnableRequestBufferingMiddleware>();
