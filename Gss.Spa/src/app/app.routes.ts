f1448fcb4a66ad26baa2862c94cbf99f46a72f94
Dmitry <dmitrymukhliada@gmail.com>
2024-11-28 02:57:37 +0100

    Add map page

--- Gss.Spa/src/app/app.routes.ts
+++ Gss.Spa/src/app/app.routes.ts
@@ -1,12 +1,12 @@
-import { MatButton } from '@angular/material/button';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
 import { MatSlider } from '@angular/material/slider';
 
 import { NotFoundComponent } from './not-found-page/not-found.component';
+import { MapComponent } from './microcontrollers/map/map.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: 'map', component: MatButton },
+    { path: 'map', component: MapComponent },
     { path: 'microcontrollers/public', component: MatSlider },
     { path: 'configuration-generator', component: MatSlideToggle },
     { path: '',   redirectTo: '/map', pathMatch: 'full' },
---
+++ Gss.Spa/src/app/microcontrollers/map/map.component.ts
@@ -0,0 +1,70 @@
+import { Component, inject, signal } from "@angular/core";
+import { LeafletModule } from '@bluehalo/ngx-leaflet';
+import { latLng, tileLayer, Map, marker, Layer, icon, Icon, latLngBounds, MapOptions, LatLngBounds } from "leaflet";
+import { MicrocontrollersQueryService } from "../core/microcontrollers-query.service";
+import { BehaviorSubject, debounceTime, distinctUntilChanged, filter, switchMap } from "rxjs";
+
+@Component({
+    selector: 'microcontrollers-map',
+    imports: [LeafletModule],
+    templateUrl: './map.component.html',
+    providers: [MicrocontrollersQueryService] // TODO: read where to inject. Scoped, single, transient
+})
+export class MapComponent {
+    title = 'Map';
+    private microcontrollerQueryService = inject(MicrocontrollersQueryService);
+    private mapBoundsSubject = new BehaviorSubject<LatLngBounds | null>(null);
+    private markerIcon = icon({
+        ...Icon.Default.prototype.options,
+        iconUrl: 'assets/marker-icon.png',
+        iconRetinaUrl: 'assets/marker-icon-2x.png',
+        shadowUrl: 'assets/marker-shadow.png'
+    });
+
+    mapLayers = signal<Layer[]>([]);
+    initMapOptions: MapOptions = {
+        layers: [
+            tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }),
+        ],
+        
+        maxZoom: 18,
+        
+        // Prevent moving out of the world bounds
+        minZoom: 3,
+        maxBounds: latLngBounds(latLng(-90, -180), latLng(90, 180)),
+        maxBoundsViscosity: 1,
+
+        // Europe's center
+        zoom: 4,
+        center: latLng(54.5260, 15.2551) 
+    };
+
+    constructor() {
+        this.mapBoundsSubject
+            .pipe(
+                filter(x => x !== null),
+                debounceTime(1000),
+                distinctUntilChanged((prev, curr) => prev.equals(curr)),
+                switchMap(bounds => {
+                    const southWest = bounds.getSouthWest();
+                    const northEast = bounds.getNorthEast();
+                    
+                    return this.microcontrollerQueryService
+                        .getMicrocontrollersMap(southWest.lat, southWest.lng, northEast.lat, northEast.lng);
+                }))
+            .subscribe(response => {
+                console.log(response);
+
+                this.mapLayers.set(response.map(x => marker([x.latitude, x.longitude], {
+                    icon: this.markerIcon
+                })));
+            });
+    }
+
+    onMapMoveEnd(map: Map) {
+        const mapBounds = map.getBounds();
+        this.mapBoundsSubject.next(mapBounds);
+    }
+}
