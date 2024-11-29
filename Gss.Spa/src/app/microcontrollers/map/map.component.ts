fbaca0f7417c5ec17ed7a1923053f74ee1027627
Dmitry <dmitrymukhliada@gmail.com>
2024-11-29 01:34:46 +0100

    Load marker on map render

--- Gss.Spa/src/app/microcontrollers/map/map.component.ts
+++ Gss.Spa/src/app/microcontrollers/map/map.component.ts
@@ -67,4 +67,9 @@ export class MapComponent {
         const mapBounds = map.getBounds();
         this.mapBoundsSubject.next(mapBounds);
     }
+
+    onMapReady(map: Map) {
+        const mapBounds = map.getBounds();
+        this.mapBoundsSubject.next(mapBounds);
+    }
 }
