d06d1066a110250b99d165a576ecaeb1a653f4b2
Dmitry <dmitrymukhliada@gmail.com>
2024-11-29 01:32:59 +0100

    Fix case

--- Gss.Spa/src/app/sensor-types/core/sensor-type.model.ts
+++ Gss.Spa/src/app/sensor-types/core/sensor-type.model.ts
@@ -3,6 +3,6 @@ import { guid } from "../../core/guid";
 export default interface SensorTypeModel {
     id: guid;
     name: string;
-    Icon: string | null;
-    Units: string | null;
+    icon: string | null;
+    units: string | null;
 }
