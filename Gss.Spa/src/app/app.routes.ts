f3e61a15d7ece453ac23e1c3f85832bf8073bffb
Dmitry <dmitrymukhliada@gmail.com>
2024-12-09 00:53:23 +0100

    Create template edit MC page and navigation to it

--- Gss.Spa/src/app/app.routes.ts
+++ Gss.Spa/src/app/app.routes.ts
@@ -6,10 +6,13 @@ import { ConfigGeneratorComponent } from './config-generator/config-generator.co
 import { SensorTypesTableComponent } from './sensor-types/sensor-types-table/sensor-types-table.component';
 import { SensorsTableComponent } from './sensors/sensors-table/sensor-table.component';
 import { UserMicrocontrollersComponent } from './microcontrollers/user-microcontrollers/user-microcontrollers.component';
+import { EditMicrocontrollerComponent } from './microcontrollers/edit-microcontroller/edit-microcontroller.component';
 
 export const routes: Routes = [
     { path: 'map', component: MicrocontrollersMapComponent },
+    { path: 'microcontrollers/create', component: EditMicrocontrollerComponent },
     { path: 'microcontrollers/:microcontrollerId', component: MicrocontrollerDetailsComponent },
+    { path: 'microcontrollers/:microcontrollerId/edit', component: EditMicrocontrollerComponent },
     { path: 'configuration-generator', component: ConfigGeneratorComponent },
     { path: 'user-microcontrollers/:userId', component: UserMicrocontrollersComponent },
     { path: 'sensors', component: SensorsTableComponent },
