f8a66319f223c4ce80c5a627123c6bbe9e609701
Dmitry <dmitrymukhliada@gmail.com>
2024-12-18 20:09:58 +0100

    Add migration to drop LastResponseTime and RequestedMicrocontrollerSensorId columns

---
+++ Gss.Infrastructure/Migrations/20241218190858_RemoveLastResponseAndRequestSensorIdColumns.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gss.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveLastResponseAndRequestSensorIdColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastResponseTime",
+                table: "Microcontrollers");
+
+            migrationBuilder.DropColumn(
+                name: "RequestedMicrocontrollerSensorId",
+                table: "Microcontrollers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastResponseTime",
+                table: "Microcontrollers",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RequestedMicrocontrollerSensorId",
+                table: "Microcontrollers",
+                type: "uuid",
+                nullable: true);
+        }
+    }
+}
